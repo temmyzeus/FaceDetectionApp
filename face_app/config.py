06bf9ab271473909a13d4e00d0864067a4aa66ae
Temiloluwa Michael <awoyeletemiloluwa@gmail.com>
2021-02-22 06:23:00 +0100

    Download function has been added!!

--- face_app/config.py
+++ face_app/config.py
@@ -1,1 +1,5 @@
+from face_app import app
+import os
+
 image_types_allowed = ["jpeg", "jpg", "png"]
+images_download_path = os.path.join(app.root_path, "static", "images")
--- face_app/routes.py
+++ face_app/routes.py
@@ -1,10 +1,11 @@
 from face_app import app
-from flask import render_template, url_for, request, flash
+from flask import render_template, url_for, request, flash, send_from_directory
 from face_app.forms import ImagesForm
 from PIL import Image
 import secrets
 import os
 from face_app.detector import detect_face
+from face_app import config
 
 def save_image(form_image):
     """Saves Image to Specified Path"""
@@ -31,3 +32,11 @@ def index():
         return render_template("index.html", title="Detect Faces", form=form, image_filename=image_filename)
 
     return render_template("index.html", title="HomePage", form=form)
+
+@app.route("/download/<path:filename>")
+def download_file(filename):
+    print("App root: ", app.root_path)
+    print("Config path: ", config.images_download_path)
+    return send_from_directory(config.images_download_path,
+                                filename,
+                                as_attachment=True)
